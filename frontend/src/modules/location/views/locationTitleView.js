adaptlearning/adapt_authoring

Add clickable breadcrumb trail

frontend/src/core/location/views/locationTitleView.js
@@ -1,41 +1,67 @@
 // LICENCE https://github.com/adaptlearning/adapt_authoring/blob/master/LICENSE
 define(function(require) {
+  var _ = require('underscore');
+  var Backbone = require('backbone');
+  var Origin = require('coreJS/app/origin');
 
-	var Origin = require('coreJS/app/origin');
-	var Backbone = require('backbone');
+  var LocationTitleView = Backbone.View.extend({
+    el: '.location-title',
 
-	var LocationTitleView = Backbone.View.extend({
+    initialize: function() {
+      this.listenTo(Origin, {
+        'location:title:update': this.render,
+        'location:title:hide': this.onHideTitle
+      });
+      this.render();
+    },
 
-		el: '.location-title',
+    render: function(data) {
+      var course = Origin.editor.data.course;
+      // add some shortcuts to common locations
+      if(data) {
+        if(data.breadcrumbs) {
+          // Dashboard
+          var dashboardI = _.indexOf(data.breadcrumbs, 'dashboard');
+          if(dashboardI > -1) {
+            data.breadcrumbs.splice(dashboardI, 1, { title: window.polyglot.t('app.dashboard'), url: '#' });
+          }
+          // Course
+          var courseI = _.indexOf(data.breadcrumbs, 'course');
+          if(courseI > -1) {
+            data.breadcrumbs.splice(courseI, 1, {
+              title: window.polyglot.t('app.editormenu'),
+              url: '#/editor/' + course.get('_id') + '/menu'
+            });
+          }
+        }
+        // set title if there isn't one
+        if(!data.title) {
+          if(data.breadcrumbs) {
+            data.title = data.breadcrumbs[data.breadcrumbs.length - 1].title;
+          } else {
+            data.title = course.get('title');
+          }
+        }
+      }
+      // render
+      var template = Handlebars.templates[this.constructor.template];
+      this.$el.html(template(data));
+      _.defer(_.bind(this.postRender, this));
 
-		initialize: function() {
-			this.listenTo(Origin, 'location:title:update', this.render);
-			this.listenTo(Origin, 'location:title:hide', this.onHideTitle);
-			this.render();
-		},
+      return this;
+    },
 
-		render: function(data) {
-		    var template = Handlebars.templates[this.constructor.template];
-		    this.$el.html(template(data));
-		    _.defer(_.bind(function() {
-		    	this.postRender();
-		    }, this));
-		    return this;
-		},
+    postRender: function() {
+      this.$('.location-title-inner').removeClass('display-none');
+      Origin.trigger('location:title:postRender', this);
+    },
 
-		postRender: function() {
-			this.$el.removeClass('display-none');
-			Origin.trigger('location:title:postRender', this);
-		},
+    onHideTitle: function() {
+      this.$('.location-title-inner').addClass('display-none');
+    }
+  }, {
+    template: 'locationTitle'
+  });
 
-		onHideTitle: function() {
-			this.$el.addClass('display-none');
-		}
-
-	}, {
-		template: 'locationTitle'
-	});
-
-	return LocationTitleView;
-
+  return LocationTitleView;
 });

ABK1-Kuul/SinqAuthoringTool

frontend/src/modules/location/views/locationTitleView.js
@@ -1,41 +1,63 @@
 // LICENCE https://github.com/adaptlearning/adapt_authoring/blob/master/LICENSE
 define(function(require) {
+  var _ = require('underscore');
+  var Backbone = require('backbone');
+  var Origin = require('core/origin');
 
-	var Origin = require('core/origin');
-	var Backbone = require('backbone');
+  var EditorDataLoader = require('modules/editor/global/editorDataLoader');
 
-	var LocationTitleView = Backbone.View.extend({
+  var LocationTitleView = Backbone.View.extend({
+    el: '.location-title',
 
-		el: '.location-title',
+    initialize: function() {
+      this.listenTo(Origin, {
+        'location:title:update': this.render,
+        'location:title:hide': this.onHideTitle
+      });
+      EditorDataLoader.waitForLoad(_.bind(this.render, this));
+    },
 
-		initialize: function() {
-			this.listenTo(Origin, 'location:title:update', this.render);
-			this.listenTo(Origin, 'location:title:hide', this.onHideTitle);
-			this.render();
-		},
+    render: function(data) {
+      var template = Handlebars.templates[this.constructor.template];
+      this.$el.html(template(this.processData(data)));
+      _.defer(_.bind(this.postRender, this));
+      return this;
+    },
 
-		render: function(data) {
-		    var template = Handlebars.templates[this.constructor.template];
-		    this.$el.html(template(data));
-		    _.defer(_.bind(function() {
-		    	this.postRender();
-		    }, this));
-		    return this;
-		},
+    processData: function(data) {
+      // nothing to do
+      if(!data || !data.breadcrumbs) {
+        return data;
+      }
+      // add some shortcuts to common locations
+      var course = Origin.editor.data.course;
+      // Dashboard
+      var dashboardI = _.indexOf(data.breadcrumbs, 'dashboard');
+      if(dashboardI > -1) {
+        data.breadcrumbs.splice(dashboardI, 1, { title: Origin.l10n.t('app.dashboard'), url: '#' });
+      }
+      // Course
+      var courseI = _.indexOf(data.breadcrumbs, 'course');
+      if(courseI > -1) {
+        data.breadcrumbs.splice(courseI, 1, {
+          title: course.get('title'),
+          url: '#/editor/' + course.get('_id') + '/menu'
+        });
+      }
+      return data;
+    },
 
-		postRender: function() {
-			this.$el.removeClass('display-none');
-			Origin.trigger('location:title:postRender', this);
-		},
+    postRender: function() {
+      this.$('.location-title-inner').removeClass('display-none');
+      Origin.trigger('location:title:postRender', this);
+    },
 
-		onHideTitle: function() {
-			this.$el.addClass('display-none');
-		}
+    onHideTitle: function() {
+      this.$('.location-title-inner').addClass('display-none');
+    }
+  }, {
+    template: 'locationTitle'
+  });
 
-	}, {
-		template: 'locationTitle'
-	});
-
-	return LocationTitleView;
-
+  return LocationTitleView;
 });