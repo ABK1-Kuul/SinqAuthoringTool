0dd3347db4e4c845a8bd5798ece0bcc2a116a66c
ABK1-Kuul <138944675+ABK1-Kuul@users.noreply.github.com>
2026-07-10 17:21:25 +0300

    chore: migrate frontend build scripts and add index route handler for application entry point

--- routes/index/index.js
+++ routes/index/index.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const configuration = require('../../lib/configuration');
 const express = require('express');
 const server = module.exports = express();
@@ -11,22 +12,8 @@ let _versions = {};
 server.set('views', __dirname);
 server.set('view engine', 'hbs');
 
-server.get('/', async function (req, res, next) {
-  const dateStamp = new Date();
-  const dateStampAsString = dateStamp.toISOString().replace(/[^0-9]/g, '');
-  const isProduction = configuration.getConfig('isProduction');
-  const useAnalytics = configuration.getConfig('useAnalytics') || false;
-  const trackingId = configuration.getConfig('trackingId') || 'UA-XXXXX-Y';
-  const versions = await getVersions();
-
-  res.render('index', Object.assign({
-    isProduction: isProduction,
-    dateStampAsString: dateStampAsString,
-    loading: app.polyglot.t('app.loading'),
-    productName: app.polyglot.t('app.productname'),
-    useAnalytics: useAnalytics,
-    trackingId: trackingId
-  }, versions));
+server.get('/', function (req, res, next) {
+  res.sendFile(path.join(configuration.getConfig('serverRoot'), 'frontend', 'build', 'index.html'));
 });
 
 async function getVersions() {
